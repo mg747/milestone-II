e3c4af46ab5ff9b58ac61fbb87761cdf0c6f9efa
mg747 <morganugiagbe77@gmail.com>
2021-06-25 09:56:16 +0000

    Added google map api function

---
+++ assets/js/maps.js
@@ -0,0 +1,37 @@
+function initMap() {
+    var map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 3,
+        center: {
+            lat: 46.619261,
+            lng: -33.134766
+        }
+    });
+
+    var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    var locations = [
+        { lat: 40.785091, lng: -73.968285 },
+        { lat: 41.084045, lng: -73.874245 },
+        { lat: 40.754932, lng: -73.984016 },
+        { lat: -37.770104, lng: 145.143299 },
+        { lat: -37.7737, lng: 145.145187 },
+        { lat: -37.774785, lng: 145.137978 },
+        { lat: -37.819616, lng: 144.968119 },
+        { lat: -38.330766, lng: 144.695692 },
+        { lat: -39.927193, lng: 175.053218 },
+        { lat: -41.330162, lng: 174.865694 },
+        { lat: -42.734358, lng: 147.439506 },
+        { lat: -42.734358, lng: 147.501315 },
+        { lat: -42.735258, lng: 147.438 },
+        { lat: -43.999792, lng: 170.463352 },
+    ];
+
+    var markers = locations.map(function(location, i) {
+        return new google.maps.Marker({
+            position: location,
+            label: labels[i % labels.length]
+        });
+    });
+
+    var markerCluster = new MarkerClusterer(map, markers, { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m' });
+}
